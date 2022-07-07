5a39985eaac05cb272f2dfdf0048680f7086f097
Alexandre Garrefa <agarrefa@blockchain.com>
2022-07-07 12:40:09 +0200

    Initial commit

---
+++ XcodeCloud/XcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudApp.swift
+//  XcodeCloud
+//
+//  Created by Alexandre Garrefa on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
